6f20481eb61dcb64c88427df42698d447c0e2111
Natalia Handerek <handerek.nat@gmail.com>
2020-11-27 16:49:56 +0100

    dodanie cmake do lab3

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/mhand/Desktop/jipp2/laboratorium3/src/matrix.cpp" "C:/Users/mhand/Desktop/jipp2/laboratorium3/build/CMakeFiles/test.dir/src/matrix.cpp.obj"
+  "C:/Users/mhand/Desktop/jipp2/laboratorium3/test/lab3.cpp" "C:/Users/mhand/Desktop/jipp2/laboratorium3/build/CMakeFiles/test.dir/test/lab3.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src"
+  ".././test"
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
